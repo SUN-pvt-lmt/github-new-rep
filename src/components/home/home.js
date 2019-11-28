b0bf5fee7e4e2377ffbd1a7aecd02badfa5dfe96
subbu <subbu86.mca@gmail.com>
2019-11-28 14:11:16 +0530

    Commit

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import RestService from '../../shared/service';
+import axios from 'axios';
 
 class Home extends Component {
 
   constructor(props) {
     super(props);
     this.restService = new RestService();
-    this.state = { count: 10, items: null, user: {} };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { count: 10, items: null, persons: [] };
   }
 
-  handleClick(e) {
-    console.log(e.target.id + 'this is wisthout:', this.state.count);
+  componentDidMount() {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      });
   }
-
-  handleClick1 = () => {
-    console.log(this.state.count);
-  };
 
   getItems = async () => {
     const urlapi = 'AccountCode';
@@ -27,19 +27,12 @@ class Home extends Component {
     return (
     <div className="grid-container" >
       <div className="grid-item">Home11{this.state.count}</div>
-
-      <button onClick={e=>this.handleClick(e)} id="handleClick">
-          Click me without
-        </button>
-
-      <button onClick={this.handleClick1} id="handleClick1">
-        Click me with
-      </button>
-
       <button onClick={this.getItems} id="getItems">
         Get Items
       </button>
-      <p></p>
+      <ul>
+        { this.state.persons.map((person, i)=> <li key={i}>{person.name}</li>)}
+      </ul>
     </div>
     );
   }
